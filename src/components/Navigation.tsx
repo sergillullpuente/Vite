9c1794d966fadab4d2aa36278258cc31f51c9075
Sergi Llull Puente <sergillullpuente@gmail.com>
2023-02-03 23:14:28 +0100

    feat: add navigator

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,42 +1,64 @@
-import Link from "./Link"
-import React from "react";
+import { useState } from 'react'
+import { Dialog } from '@headlessui/react'
 
 const NAVIGATION_LINKS = [
-  {
-    title: "Inici",
-    href: "/"},
-  {
-    title: "Informació bàsica",
-    href: "/info"
-  },
-  {
-    title: "On dormir",
-    href: "/sleep"
-  },
-  {
-    title: "Fotos",
-    href: "/photos"
-  },
-  {
-    title: "Assistencia",
-    href: "/assistance"
-  },
+  { name: 'Inici', href: '/' },
+  { name: 'Informació bàsica', href: '/info' },
+  { name: 'On dormir', href: '/sleep' },
+  { name: 'Fotos', href: '/photos' },
+  { name: 'Assistencia', href: '/assistance' },
 ]
 
-function Navigation () {
+export default function Navigation() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   return (
-    <header>
-      <nav className='flex items-center p-6 text-center lg:justify-center' id='navbar'>
-        <ul className='flex items-center space-x-4'>
-          {
-            NAVIGATION_LINKS.map(({title, href}) => {
-              return <li><Link href={href} title={title} disabled={false}/></li>
-            })
-          }
-        </ul>
-      </nav>
-    </header>
+      <div className="isolate bg-white">
+        <div className="px-6 pt-6 lg:px-8">
+          <nav className="flex items-center lg:justify-center justify-between" aria-label="Global">
+            <div className="flex lg:hidden">
+              <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" onClick={() => setMobileMenuOpen(true)}>
+                <span className="sr-only">Open main menu</span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5" />
+                </svg>
+              </button>
+            </div>
+            <div className="hidden lg:flex lg:gap-x-12">
+              {NAVIGATION_LINKS.map((item) => (
+                  <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+                    {item.name}
+                  </a>
+              ))}
+            </div>
+          </nav>
+          <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+            <Dialog.Panel focus="true" className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden">
+              <div className="flex items-center justify-between">
+                <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700" onClick={() => setMobileMenuOpen(false)}>
+                  <span className="sr-only">Close menu</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                  </svg>
+                </button>
+              </div>
+              <div className="mt-6 flow-root">
+                <div className="-my-6 divide-y divide-gray-500/10">
+                  <div className="space-y-2 py-6">
+                    {NAVIGATION_LINKS.map((item) => (
+                        <a
+                            key={item.name}
+                            href={item.href}
+                            className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
+                        >
+                          {item.name}
+                        </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </Dialog.Panel>
+          </Dialog>
+        </div>
+      </div>
   )
 }
-
-export default Navigation
