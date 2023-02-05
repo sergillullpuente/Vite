4a46875e255682cbe22252a7429196f1293d5706
Sergi Llull Puente <sergillullpuente@gmail.com>
2023-02-05 18:41:57 +0100

    feat: add background to char and style home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,14 @@
+const DAY = 1000 * 60 * 60 * 24;
+const daysUntil = (date: Date, otherDate: Date) =>
+    Math.ceil(Math.abs(date.valueOf() - otherDate.valueOf()) / DAY);
+
 function Home() {
     return (
       <main>
-        <section id='details'>
-        </section>
-        <section id='rsvp'>
+        <section id='days' className='flex flex-col items-center text-center align-middle font-yanone tracking-widest text-3xl font-normal text-green-900 pt-5 gap-3'>
+            <div>ENS CASEM!</div>
+            <div>FALTEN {daysUntil(new Date(), new Date('2023-09-23'))} DIES</div>
+            <div className='font-ebg text-xl'>Finca La Baumeta, La Quar, Berguedà</div>
         </section>
       </main>
     )
