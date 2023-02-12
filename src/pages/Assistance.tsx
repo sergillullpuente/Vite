958ad68ed5f0e44f6e322a69f0c4b2ffa03d89e6
Sergi Llull Puente <sergillullpuente@gmail.com>
2023-02-12 23:45:25 +0100

    feat: center form

--- src/pages/Assistance.tsx
+++ src/pages/Assistance.tsx
@@ -1,7 +1,7 @@
 function Assistance() {
     return(
         <main>
-            <section className='flex flex-col items-center w-full overflow-hidden relative' id='assistance'>
+            <section className='flex items-center w-full overflow-hidden relative' id='assistance'>
                 <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScDcUaTgGD72utHSf88ZAds036xP8grj51dLrydhuB5jEwR9Q/viewform?embedded=true" width="640" height="1086">S&#39;està carregant…</iframe>
             </section>
         </main>
