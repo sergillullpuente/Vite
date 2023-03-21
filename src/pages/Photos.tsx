dcbf6ed363f738811d9be5db8dd436cf716e6f25
Sergi Llull Puente <sergillullpuente@gmail.com>
2023-03-21 19:30:42 +0100

    feat: fix env naming

--- src/pages/Photos.tsx
+++ src/pages/Photos.tsx
@@ -17,9 +17,9 @@ const firebaseConfig = {
     authDomain: process.env.AUTH_DOMAIN,
     projectId: process.env.PROJECT_ID,
     storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDERID,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
     appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMEN_TID,
+    measurementId: process.env.MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
