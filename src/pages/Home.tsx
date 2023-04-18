dda6bd42c08324957e2302d90d626e9ebb0d15d1
Sergi Llull Puente <sergillullpuente@gmail.com>
2023-04-18 18:39:50 +0200

    feat: remove emojis, add correct path to ico

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,8 +6,8 @@ function Home() {
     return (
       <main className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
         <section id='days' className='flex flex-col items-center text-center align-middle font-yanone tracking-widest text-3xl font-normal text-green-900 pt-5 gap-5'>
-            <div>👰🤵 ENS CASEM!</div>
-            <div>FALTEN {daysUntil(new Date(), new Date('2023-09-23'))} DIES 🗓️</div>
+            <div>ENS CASEM!</div>
+            <div>FALTEN {daysUntil(new Date(), new Date('2023-09-23'))} DIES </div>
             <video className="blur-in-expand rounded-lg w-full h-full object-cover" autoPlay loop muted playsInline>
                 <source src="/video-web.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
--- src/pages/Sleep.tsx
+++ src/pages/Sleep.tsx
@@ -3,7 +3,7 @@ import Heading from "../components/Heading";
 const Sleep = () => {
     return (
         <Heading
-            title='Passar la nit amb haymas ⛺'
+            title='Passar la nit amb haymas'
             subtitle="Per als que es plantejen passar la nit, ho tenim tot cobert! Estem encantats d'anunciar que afegirem haymas al cobert de la masia per oferir una solució còmode i fàcil. Les haymas són una forma estupenda de relaxar-se després d'una llarga nit de festa. Només cal que ens ho comuniqueu amb antelació si teniu previst passar la nit perquè puguem fer els preparatius necessaris."
             imageSrc="/haymas.jpg"
         />
