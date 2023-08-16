765b61b5a1475d54fad4509538af88378aa7f57c
Sergi Llull Puente <sergillullpuente@gmail.com>
2023-08-16 16:11:36 +0200

    feat: use muted intro video and remove unnecessari /public

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,9 +8,7 @@ function Home() {
         <section id='days' className='flex flex-col items-center text-center align-middle font-yanone tracking-widest text-3xl font-normal text-green-900 pt-5 gap-5'>
             <div>ENS CASEM!</div>
             <div>FALTEN {daysUntil(new Date(), new Date('2023-09-23'))} DIES </div>
-            <video className="blur-in-expand rounded-lg w-full h-full object-cover" autoPlay loop muted playsInline>
-                <source src="/video-web.mp4" type="video/mp4" />
-                    Your browser does not support the video tag.
+            <video src="/video_mute.mp4" preload="metadata" className="blur-in-expand rounded-lg w-full h-full object-cover" autoPlay loop muted playsInline>
             </video>
         </section>
       </main>
